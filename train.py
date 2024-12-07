2815d1d623441c435a0a360ea44cce2231563aa5
Riabinin Evgeny <95548107+Eriabinin777@users.noreply.github.com>
2024-12-07 23:33:20 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,24 @@
+import hydra
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from modules.data_module import MNISTDataModule
+from modules.model import MNISTClassifier
+from omegaconf import DictConfig
+
+
+@hydra.main(version_base="1.3", config_path="conf", config_name="config")
+def train(cfg: DictConfig):
+    pl.seed_everything(45)
+
+    # Инициализация
+    data_module = MNISTDataModule(cfg.data.data_dir, cfg.data.batch_size)
+    model = MNISTClassifier(int(cfg.model.input_size), int(cfg.model.num_classes), cfg.model.lr)
+    logger = TensorBoardLogger(save_dir=cfg.logger.save_dir,name=cfg.logger.name)
+
+    trainer = Trainer(logger=logger, **cfg.trainer)
+    trainer.fit(model, data_module)
+
+
+if __name__ == "__main__":
+    train()
